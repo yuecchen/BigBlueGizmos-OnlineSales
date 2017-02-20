629acf1d833d6e2e03343cc340833574dd75cfa2
b1stern <bstern@us.ibm.com>
2017-02-20 14:57:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //------------------------------------------------------------------------------
 // hello world app is based on node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------ 
 
 // This application uses express as its web server 
 // for more info, see: http://expressjs.com

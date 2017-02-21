e8223139b4d7f691863b5964b9f0d4d49721525d
b1stern <bstern@us.ibm.com>
2017-02-21 11:52:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express'); 
-
+ 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv'); 
